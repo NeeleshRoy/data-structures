e1dc58c69eca576a21dba69c75759b77b3040451
neeleshroy <neeleshroy.2013@gmail.com>
2018-04-17 21:33:59 +0530

    lint fixes

--- test/PriorityQueueSpec.js
+++ test/PriorityQueueSpec.js
@@ -21,32 +21,34 @@ describe('Priority Queue - Class and Methods', () => {
   describe('Priority Queue.functions', () => {
     it('enqueue() Should add the element in the queue correctly', () => {
       const pqueue = new PriorityQueue();
-      pqueue.enqueue("Neelesh", 1);
-      expect(pqueue.datastore).to.eql([{ value: "Neelesh", priority: 1 }]); 
+      pqueue.enqueue('Neelesh', 1);
+      expect(pqueue.datastore).to.eql([{ value: 'Neelesh', priority: 1 }]);
     });
 
     it('enqueue() Should add the element in the queue correctly', () => {
       const pqueue = new PriorityQueue();
-      pqueue.enqueue("Neelesh", 1);
-      pqueue.enqueue("diya", 4);
-      expect(pqueue.datastore).to.eql([{ value: "Neelesh", priority: 1 }, { value: "diya", priority: 4 }]); 
+      pqueue.enqueue('Neelesh', 1);
+      pqueue.enqueue('diya', 4);
+      expect(pqueue.datastore).to.eql([{ value: 'Neelesh', priority: 1 },
+      { value: 'diya', priority: 4 }]);
     });
 
     it('dequeue() Should delete the element in the queue correctly', () => {
       const pqueue = new PriorityQueue();
-      pqueue.enqueue("Neelesh", 2);
-      pqueue.enqueue("diya", 3);
-      pqueue.enqueue("rash", 1);
+      pqueue.enqueue('Neelesh', 2);
+      pqueue.enqueue('diya', 3);
+      pqueue.enqueue('rash', 1);
       pqueue.dequeue();
-      expect(pqueue.datastore).to.eql([{ value: "Neelesh", priority: 2 }, { value: "diya", priority: 3 }]); 
+      expect(pqueue.datastore).to.eql([{ value: 'Neelesh', priority: 2 },
+      { value: 'diya', priority: 3 }]);
     });
 
     it('toString() Should return the datastore', () => {
       const pqueue = new PriorityQueue();
-      pqueue.enqueue("Neelesh", 2);
-      pqueue.enqueue("diya", 3);
+      pqueue.enqueue('Neelesh', 2);
+      pqueue.enqueue('diya', 3);
       const out = pqueue.toString();
-      expect(out).to.eql([{ value: "Neelesh", priority: 2 }, { value: "diya", priority: 3 }]); 
+      expect(out).to.eql([{ value: 'Neelesh', priority: 2 }, { value: 'diya', priority: 3 }]);
     });
   });
 });
