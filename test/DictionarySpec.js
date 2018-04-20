32860e818d0e65adfea720f18a0e414a318aff18
neeleshroy <neeleshroy.2013@gmail.com>
2018-04-21 01:19:58 +0530

    tests

--- test/DictionarySpec.js
+++ test/DictionarySpec.js
@@ -9,6 +9,7 @@
 
 import { expect } from 'chai';
 import Dictionary from '../src/Dictionary/Dictionary';
+import Problem1 from '../src/Dictionary/problem-1';
 
 describe('Dictionary - Class and Methods', () => {
   describe('Dictionary.properties', () => {
@@ -87,6 +88,49 @@ describe('Dictionary - Class and Methods', () => {
       expect(keys[0]).to.eql('aaa');
       expect(keys[1]).to.eql('abc');
       expect(keys[2]).to.eql('foo');
+    });
+  });
+
+  describe('Dictionary.question-1', () => {
+    it('Should Populate the data properly', () => {
+      const solution = new Problem1();
+      solution.populateData();
+      expect(solution.dict.count()).to.eql(100);
+    });
+
+    it('Get the phone number', () => {
+      const solution = new Problem1();
+      solution.populateData();
+      const out = solution.getPhone('Emmet McCudden');
+      expect(out).to.eql('424-602-5213');
+    });
+
+    it('Add the phone number', () => {
+      const solution = new Problem1();
+      solution.populateData();
+      solution.addPhone('Neelesh Roy', '991-827-9374');
+
+      expect(solution.dict.count()).to.eql(101);
+      expect(solution.dict.datastore['Neelesh Roy']).to.exist;
+      expect(solution.dict.datastore['Neelesh Roy']).to.eql('991-827-9374');
+    });
+
+    it('Remove the phone number', () => {
+      const solution = new Problem1();
+      solution.populateData();
+      solution.removePhone('Sherm Glanton');
+
+      expect(solution.dict.count()).to.eql(99);
+      expect(solution.dict.datastore['Sherm Glanton']).to.not.exist;
+    });
+
+    it('should clear all the data', () => {
+      const solution = new Problem1();
+      solution.populateData();
+      solution.clearAll();
+
+      expect(solution.dict.count()).to.eql(0);
+      expect(solution.dict.datastore).to.eql([]);
     });
   });
 });
